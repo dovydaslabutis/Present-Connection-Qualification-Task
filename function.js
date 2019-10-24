2f324e02b382e3df5a1e390e1febcbc41484d9c8
Dovydas Labutis <dovydaslab@gmail.com>
2019-10-24 12:32:19 +0300

    console.log working

--- function.js
+++ function.js
@@ -8,12 +8,15 @@ document.getElementById('getText').addEventListener('click', getText);
 //     })
 // }
 
+
 function getText(){
     fetch('sample.txt')
     .then(function(res){
+        console.log ("pass results");
         return res.text();
     })
     .then(function(data){
+        console.log ("pass data");
         console.log(data)
     })
 }
