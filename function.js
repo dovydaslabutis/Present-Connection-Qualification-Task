949d9f2d12a6bb76c74fd36699e887045c198413
Dovydas Labutis <dovydaslab@gmail.com>
2019-10-24 12:54:16 +0300

    output works

--- function.js
+++ function.js
@@ -1,22 +1,23 @@
 document.getElementById('getText').addEventListener('click', getText);
 
+function getText(){
+    fetch('sample.txt')
+    .then((res) => res.text())
+    .then((data) => {
+        document.getElementById('output').innerHTML = data;
+    })
+}
+
+
 // function getText(){
 //     fetch('sample.txt')
-//     .then((res) => res.text())
-//     .then((data) => {
-//         document.getElementById('output').innerHTML = data;
+//     .then(function(res){
+//         console.log ("pass results");
+//         return res.text();
+//     })
+//     .then(function(data){
+//         console.log ("pass data");
+//         console.log(data)
 //     })
 // }
 
-
-function getText(){
-    fetch('sample.txt')
-    .then(function(res){
-        console.log ("pass results");
-        return res.text();
-    })
-    .then(function(data){
-        console.log ("pass data");
-        console.log(data)
-    })
-}
