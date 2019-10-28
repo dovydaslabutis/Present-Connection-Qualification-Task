b6bdd0b7ab8b08ffb5e628be9c28e3c94a033bf1
Dovydas Labutis <dovydaslab@gmail.com>
2019-10-28 10:47:57 +0200

    searching how to solve the second page problem

--- function.js
+++ function.js
@@ -17,25 +17,4 @@ function getPosts(){
     })
 }
 
-document.getElementById('getPosts').addEventListener('click', addPost);
 
-ID_by_row();
-function ID_by_row(){
-    fetch('https://jsonplaceholder.typicode.com/posts/%7bID%7d')
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data)
-        let output = '<h2>Is the form you were looking for ? </h2>';
-        data.forEach(function(post){
-            output += `
-             <div>
-                 <p>${post.userId}</p>
-                 <p>${post.id}</p>
-                 <p>${post.title}</p>
-                 <p>${post.body}</p>
-             </div>
-            `;
-        });
-        document.getElementById('ID_by_row').innerHTML = output;
-    })
-}
